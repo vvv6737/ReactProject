bfae1a651cc6f2124c1e61e552819c862a252c70
jeonghoon <vvv6737@naver.com>
2021-09-07 01:24:03 +0900

    update

--- src/main/java/com/react/application/ReactApplication.java
+++ src/main/java/com/react/application/ReactApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.react.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ReactApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ReactApplication.class, args);
 	}
 
 }
